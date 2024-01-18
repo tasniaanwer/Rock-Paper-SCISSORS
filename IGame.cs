e47771d1dc97bb7118ce028289fc8768fdaf21f9
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2024-01-18 22:24:30 +0600

    Add files via upload

---
+++ IGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REFACTORED_ROCK_PAPER
+{
+    interface IGame
+    {
+        void Play();
+    }
+}
