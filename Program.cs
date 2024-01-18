c8a13174efd5c418178bfd3cdc61438742f8a305
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2024-01-18 22:23:52 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using REFACTORED_ROCK_PAPER;
+using System;
+
+namespace MyFirstProgram
+{
+    
+
+    
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGame rockPaperScissorsGame = new RockPaperScissorsGame();
+            rockPaperScissorsGame.Play();
+
+            IGame ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.Play();
+        }
+    }
+}
