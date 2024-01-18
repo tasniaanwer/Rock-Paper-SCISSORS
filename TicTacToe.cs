5ee12ca6c17ed09b7a611cc646044a8394e6f791
tasniaanwer <117499239+tasniaanwer@users.noreply.github.com>
2024-01-18 22:25:32 +0600

    Add files via upload

---
+++ TicTacToe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REFACTORED_ROCK_PAPER
+{
+    class TicTacToeGame : IGame
+    {
+        public void Play()
+        {
+            // Implementation for Tic Tac Toe game
+            Console.WriteLine("Playing Tic Tac Toe...");
+        }
+    }
+}
